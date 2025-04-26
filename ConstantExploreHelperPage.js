b81c9b996f50012b60d7eb558f071d2b2afcee41
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2025-04-26 23:31:37 +0530

    website design and development
    added website for event management company in project catalog.

--- ConstantExploreHelperPage.js
+++ ConstantExploreHelperPage.js
@@ -25,7 +25,7 @@ const project_type_indices = {
     "Graphics Designing": [2, 3, 4, 1],
     "Business Analytics": [24, 1],
     "Business Consulting": [1],
-    "Website Development": [1],
+    "Website Development": [11, 1],
     "Software Development": [1],
     "Mobile Apps": [1],
     "AI Enabled": [5, 6, 7, 1],
