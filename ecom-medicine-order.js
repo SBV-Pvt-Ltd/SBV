48067d453d0cc8ca9502b9d89990373a88b5dac3
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2025-09-12 15:06:12 +0530

    Added ecom - medicine orders
    For Binod Nana.

---
+++ ecom-medicine-order.js
@@ -0,0 +1,19 @@
+﻿youtube= "";
+whatsApp= "";
+twitter= "";
+linkedIn= "";
+facebook= "";
+google_business= "";
+website= "";
+threads= "";
+instagram = "";
+
+// Configuration - 917903854010
+const CONFIG = {
+    whatsappNumber: "+919168669610",
+    spreadsheetId: '1ZilazHW44UHB03semgMtwA2Qfhn3OROgWqC6GPPW6ro',
+    apiKey: 'AIzaSyDKK039v7qBCELSM0KqMYZYJAGDm4wt_V0',
+    range: 'Form responses 1!A2:J'
+};
+
+last_updated_on = "August 12, 2025"; 
