dc2f775bc8c8072d5c4828631adadd9bd0a958ef
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2026-02-01 14:06:08 +0530

    New index design push.
    Added removed and renamed files.

--- Constant.js
+++ Constant.js
@@ -9,32 +9,33 @@ threads= "";
 instagram = "https://www.instagram.com/saharshbhadaniventure/";
 blogs="https://bhadani-saharsh.github.io/blog.saharsh.bhadani/";
 features_service_page_name="FeaturesServices.html";
-index_page_name="index.html";
-faq_page_name="faq.html";
-policy_page_name = "policy.html";
-tnc_page_name = "TermsAndConditions.html";
-cnr_page_name = "CancellationAndRefund.html";
-snd_page_name = "ShippingAndDelivery.html";
+index_page_name = "index.html";
+explore_page_name = "explore.html";
+explore_page_name_project_section = "explore.html#project";
 
-business_analytics_page = "business_analytics.html";
-ai_ml_page = "";
-it_consultation_page = "";
-graphics_designing_page = "Graphics_design.html";
-webdesigning_page = "";
-app_development_page = "";
-software_development_page = "";
-about_us_page = "";
+faq_page_name="link-faq.html";
+policy_page_name = "link-policy.html";
+tnc_page_name = "link-TermsAndConditions.html";
+cnr_page_name = "link-CancellationAndRefund.html";
+snd_page_name = "link-ShippingAndDelivery.html";
+payments_page_name = "link-payments.html";
+careers_page_name = "link-careers.html";
 
-payments_page_name="payments.html";
-careers_page_name="careers.html";
-explore_page_name="explore.html";
-explore_page_name_project_section="explore.html#project";
+business_analytics_page = "page-business_analytics.html";
+ai_ml_page = "page-AI-ML.html";
+it_consultation_page = "page-it-consultation.html";
+graphics_designing_page = "page-graphics_design.html";
+webdesigning_page = "page-webdesigning.html";
+app_development_page = "page-mobile-app-development.html";
+software_development_page = "page-software-development.html";
+about_us_page = "page-aboutUs.html";
+
 save_data_db_url="https://django-hello-world-pink-ten.vercel.app";
 working_local = false;
 year_to_display_along_with_copyright = 2026;
 
 
-last_updated_on = "January 03, 2026";
+last_updated_on = "February 01, 2026";
 const youtube_videos = [
     "https://www.youtube.com/embed/Ag995qpzNlw", 
     "https://www.youtube.com/embed/O9cf_yC0pGo",
